17c2482a328a8b83ee58983fb77f549da4faf6e3
DiesIrae99 <149577355+DiesIrae99@users.noreply.github.com>
2023-11-15 11:21:59 +0100

    Fixed volume and start/restart

--- Mushroom_Madness/Mushroom_Madness/Assets/Scripts/ApplicationHandler.cs
+++ Mushroom_Madness/Mushroom_Madness/Assets/Scripts/ApplicationHandler.cs
@@ -7,10 +7,12 @@ using UnityEngine.SceneManagement;
 // It can be used together with UnityEventOnTrigger, or UI-button-events, to decide when a scene should be changed or the game should be closed
 public class ApplicationHandler : MonoBehaviour
 {
+    public string Scenename;
+
     public void ChangeScene()
     {
         // Load the scene named "Mushroom_Madness"
-        SceneManager.LoadSceneAsync("Mushroom_Madness", LoadSceneMode.Single);
+        SceneManager.LoadSceneAsync(Scenename, LoadSceneMode.Single);
     }
 
     public void QuitGame()
